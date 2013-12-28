f94d292a50b0d9f527845fdc64ef2a48f2e68254
Carlos Cervantes <CCER562@yahoo.com>
2013-12-27 20:04:08 -0800

    Insertion sort part 2 update

--- ruby/insertion_sort_part2.rb
+++ ruby/insertion_sort_part2.rb
@@ -1,16 +1,12 @@
 #sort the entire arrway
 
 def insertionSort(ar)
-	#ar.each_with_index do |val, i|
 	for i in 1..(ar.length - 1)
 		item = ar[i]
-		hole = i - 0 #ar.length - 1
+		hole = i - 0
 		while ar[hole - 1] > item && hole > 0
-			#print "While #{ar[hole - 1]} > #{item} \n"
 			ar[hole] = ar[hole - 1]
-			#print "Switch #{ar[hole]} with #{ar[hole - 1]} \n"
 			hole = hole - 1
-			#print_array ar
 		end
 		ar[hole] = item
 		print_array ar
